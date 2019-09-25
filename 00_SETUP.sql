bc5eaad84ac68c884ae576bd3dcb0068ddb86e82
Mark Leung <leungas@Marks-MacBook-Pro.local>
2019-09-25 08:17:22 +0800

    Cater for local host and other source to access

--- 00_SETUP.sql
+++ 00_SETUP.sql
@@ -7,6 +7,7 @@ CREATE USER 'zimplidev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Z1
 CREATE USER 'zimplidev'@'192.168.56.%' IDENTIFIED WITH mysql_native_password BY 'Z1mPid*V';
 
 CREATE USER 'zimplify'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Z1mPlify';
+CREATE USER 'zimplify'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY 'Z1mPlify';
 CREATE USER 'zimplify'@'172.31.%.%' IDENTIFIED WITH mysql_native_password BY 'Z1mPlify';
 CREATE USER 'zimplify'@'192.168.56.%' IDENTIFIED WITH mysql_native_password BY 'Z1mPlify';
 
@@ -14,20 +15,27 @@ USE ZIMPLIFY;
 
 -- FOR LOCAL ACCESS FOR DBA
 GRANT ALL ON ZIMPLIFY.* TO 'zimplidba'@'localhost';
+GRANT ALL ON ZIMPLIFY.* TO 'zimplidba'@'localhost';
 
 -- FOR REMOTE DEV ACCESS - DO NOT USE THIS FOR LIVE
 GRANT ALL ON ZIMPLIFY.* TO 'zimplidev'@'192.168.56.%';
 
 -- THIS IS FOR THE PROVIDER ACCESS
+GRANT SELECT ON ZIMPLIFY.* TO 'zimplify'@'localhost';
+GRANT UPDATE ON ZIMPLIFY.* TO 'zimplify'@'localhost';
+GRANT DELETE ON ZIMPLIFY.* TO 'zimplify'@'localhost';
+GRANT EXECUTE ON ZIMPLIFY.* TO 'zimplify'@'localhost';
+
+GRANT SELECT ON ZIMPLIFY.* TO 'zimplify'@'127.0.0.1';
+GRANT UPDATE ON ZIMPLIFY.* TO 'zimplify'@'127.0.0.1';
+GRANT DELETE ON ZIMPLIFY.* TO 'zimplify'@'127.0.0.1';
+GRANT EXECUTE ON ZIMPLIFY.* TO 'zimplify'@'127.0.0.1';
+
 GRANT SELECT ON ZIMPLIFY.* TO 'zimplify'@'192.168.56.%';
 GRANT UPDATE ON ZIMPLIFY.* TO 'zimplify'@'192.168.56.%';
 GRANT DELETE ON ZIMPLIFY.* TO 'zimplify'@'192.168.56.%';
 GRANT EXECUTE ON ZIMPLIFY.* TO 'zimplify'@'192.168.56.%';
 
-GRANT SELECT ON ZIMPLIFY.* TO 'zimplifydba'@'192.168.56.%';
-GRANT UPDATE ON ZIMPLIFY.* TO 'zimplifydba'@'192.168.56.%';
-GRANT DELETE ON ZIMPLIFY.* TO 'zimplifydba'@'192.168.56.%';
-GRANT EXECUTE ON ZIMPLIFY.* TO 'zimplifydba'@'192.168.56.%';
 
 -- NOW MAKE SURE WE FLUSH
 FLUSH PRIVILEGES;
